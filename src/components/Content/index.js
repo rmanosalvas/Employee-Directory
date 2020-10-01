1fc53a5fa74043acf53f5052d2c0297542f2ec38
Ricardo <manosalvas2013@gmail.com>
2020-10-01 12:24:44 -0400

    employees added

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import employees from '../employees.json';
 
-function Content() {
-    return(
-        <div>
-            <p>Input search bar here</p>
-            <table className='table table-striped table-hover'>
-                <thead className='thead-secondary'>
-                    <tr>
-                        <th>Profile Pic</th>
-                        <th>Name</th>
-                        <th>DOB</th>
-                        <th>Position</th>
-                        <th>Phone Number</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-            </table>
-        </div>
-    );
+class Content extends Component {
+
+    state = {
+        employees
+    };
+
+
+    render () {
+        return(
+            <div>
+                <p>Input search bar here</p>
+                <table className='table table-striped table-hover'>
+                    <thead className='thead-secondary'>
+                        <tr>
+                            <th>Profile Pic</th>
+                            <th>Name</th>
+                            <th>DOB</th>
+                            <th>Position</th>
+                            <th>Phone Number</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.employees.map(employee => {
+                            return (
+                                <tr key={employee.id}>
+                                    <td>
+                                        {/* <img alt='pic' src={employee.image}></img> */}
+                                    </td>
+                                    <td>{employee.name}</td>
+                                    <td>{employee.dob}</td>
+                                    <td>{employee.occupation}</td>
+                                    <td>{employee.phone}</td>
+                                    <td>{employee.email}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
 };
 
 export default Content;
