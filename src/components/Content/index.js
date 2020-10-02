f5c02063677228303c0fb8aa060f7a73c846ad6c
Ricardo <manosalvas2013@gmail.com>
2020-10-01 23:16:01 -0400

    position change created

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,28 +4,43 @@ import React, { Component } from 'react';
 export default class Content extends Component {
 
     state = {
-        employees: []
-        // allEmployees: [],
-        // isLoading: true,
+        employees: [],
+        allEmployees: []
     };
 
     componentDidMount() {
-        fetch("../employees.json")
+        fetch('../employees.json')
         .then(function (response) {
             return response.json();
           })
         .then((response) => {
             this.setState({
-                employees: response
-                // allEmployees: response,
-                // isLoading: false
+                employees: response,
+                allEmployees: response
             })
         })
         .catch((error) => {
             console.log(error)
         });
-    }
-    
+    };
+
+
+    sortPosition = (event) => {
+        const position = event.target.value;
+        if (position === 'All') {
+            this.setState({ employees: this.state.allEmployees });
+        } else {
+            this.setState({
+                employees: this.state.allEmployees.filter(function (employee) {
+                    if (employee.team === position) {
+                        return true;
+                    }
+                    return false;
+                  }),
+            });
+        }
+    };
+
     sortDob = (event) => { 
         const dob = event.target.value;
         if (dob === 'Oldest') {
@@ -52,17 +67,28 @@ export default class Content extends Component {
             <div>
                 <p>Input search bar here</p>
                 <div>
+                    <label>Sort by position:</label>
+                    <select onChange={this.sortPosition} id='position'>
+                        <option value='All'>All</option>
+                        <option value='Senior Dev'>Senior Dev</option>
+                        <option value='Front End'>Front End</option>
+                        <option value='Back End'>Back End</option>
+                        <option value='Full Stack'>Full Stack</option>
+                        <option value='Junior'>Junior Dev</option>
+                    </select>
+                </div>
+                <div>
                     <label>Sort by birth date:</label>
                         <select onChange={this.sortDob}>
                             <option>Default</option>
-                            <option value="Oldest">Oldest</option>
-                            <option value="Youngest">Youngest</option>
+                            <option value='Oldest'>Oldest</option>
+                            <option value='Youngest'>Youngest</option>
                         </select>
                 </div>
                 <table className='table table-striped table-hover'>
                     <thead className='thead-secondary'>
                         <tr>
-                            <th>Profile Pic</th>
+                            {/* <th>Profile Pic</th> */}
                             <th>Name</th>
                             <th>DOB</th>
                             <th>Position</th>
@@ -74,12 +100,9 @@ export default class Content extends Component {
                         {this.state.employees.map(function (employee) {
                             return (
                                 <tr key={employee.id}>
-                                    <td>
-                                        {/* <img alt='pic' src={employee.image}></img> */}
-                                    </td>
                                     <td>{employee.name}</td>
                                     <td>{employee.dob}</td>
-                                    <td>{employee.occupation}</td>
+                                    <td>{employee.team}</td>
                                     <td>{employee.phone}</td>
                                     <td>{employee.email}</td>
                                 </tr>
