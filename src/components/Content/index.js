13085ca6174fe84abfab7739695f8cc426bab78c
Ricardo <manosalvas2013@gmail.com>
2020-10-01 22:08:30 -0400

    DOB organizer

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react';
-import employees from '../employees.json';
+// import employees from '../employees.json';
 
-class Content extends Component {
+export default class Content extends Component {
 
     state = {
-        employees
+        employees: []
+        // allEmployees: [],
+        // isLoading: true,
     };
 
+    componentDidMount() {
+        fetch("../employees.json")
+        .then(function (response) {
+            return response.json();
+          })
+        .then((response) => {
+            this.setState({
+                employees: response
+                // allEmployees: response,
+                // isLoading: false
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+    }
+    
+    sortDob = (event) => { 
+        const dob = event.target.value;
+        if (dob === 'Oldest') {
+            this.setState({
+                employees: this.state.employees.sort(function (a, b) {
+                    var dateA = new Date(a.dob).getTime();
+                    var dateB = new Date(b.dob).getTime();
+                    return dateA > dateB ? 1 : -1;
+                  })
+            });
+        } else if (dob === 'Youngest') {
+            this.setState({
+                employees: this.state.employees.sort(function (a, b) {
+                    var dateA = new Date(a.dob).getTime();
+                    var dateB = new Date(b.dob).getTime();
+                    return dateA < dateB ? 1 : -1;
+                  })
+            }); 
+        }
+     };
 
     render () {
         return(
             <div>
                 <p>Input search bar here</p>
+                <div>
+                    <label>Sort by birth date:</label>
+                        <select onChange={this.sortDob}>
+                            <option>Default</option>
+                            <option value="Oldest">Oldest</option>
+                            <option value="Youngest">Youngest</option>
+                        </select>
+                </div>
                 <table className='table table-striped table-hover'>
                     <thead className='thead-secondary'>
                         <tr>
@@ -24,7 +71,7 @@ class Content extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.employees.map(employee => {
+                        {this.state.employees.map(function (employee) {
                             return (
                                 <tr key={employee.id}>
                                     <td>
@@ -44,8 +91,6 @@ class Content extends Component {
         );
     }
 
-};
-
-export default Content;
+}
 
 
