8d38d35f58292dcbffc932387a02fdaaa0fce63c
Ricardo <manosalvas2013@gmail.com>
2020-10-20 21:47:48 -0400

    Updates

---
+++ precache-manifest.2da9261ee605f5b89555363c489880f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29634d5aa055cd013ec4ead8ea46e99f",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "ae88ec1daaeca7d274d0",
+    "url": "/Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "aeff3127628c689d943c",
+    "url": "/Employee-Directory/static/css/main.984dfad9.chunk.css"
+  },
+  {
+    "revision": "ae88ec1daaeca7d274d0",
+    "url": "/Employee-Directory/static/js/2.b60e463d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.b60e463d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeff3127628c689d943c",
+    "url": "/Employee-Directory/static/js/main.a93b2b93.chunk.js"
+  },
+  {
+    "revision": "88776d5770f0e6852f66",
+    "url": "/Employee-Directory/static/js/runtime-main.28620774.js"
+  }
+]);
